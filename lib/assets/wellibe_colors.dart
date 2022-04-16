d4517ca7a039f899b207b7bbf20f9d1069800e3b
Shaked Nissanov <shaked.nissanov@gmail.com>
2022-04-16 23:10:11 +0300

    Basic QR scanning scans QR code with name and displays it as seen in the pdf.

---
+++ lib/assets/wellibe_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color mainYellow = Colors.yellow[50];
+  static const Color mainWhite = Colors.white;
+  static const Color mainGrey = Colors.grey;
+  static const Color header = Colors.black;
+  static Color buttonRed = Colors.red[200];
+}
---
+++ lib/qr_scanning_page.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:barcode_scan2/barcode_scan2.dart';
+
+import 'package:wellibe_app/assets/wellibe_colors.dart';
+import 'package:wellibe_app/top_bar.dart';
+
+const String scanQRString = 'סריקת רופא';
+const String cancelQRScanString = 'ביטול סריקה';
+
+class QRScanningPage extends StatefulWidget {
+  const QRScanningPage({Key key}) : super(key: key);
+
+  @override
+  State<QRScanningPage> createState() => _QRScanningPageState();
+}
+
+class _QRScanningPageState extends State<QRScanningPage> {
+  Future<void> scanQR() async {
+    var result = await BarcodeScanner.scan();
+
+    print(result.type); // The result type (barcode, cancelled, failed)
+    if(result.type == ResultType.Barcode) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => DoctorInfoPage(doctorName: result.rawContent)),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: TopBar(),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: AppColors.mainWhite,
+              child: Center(
+                child: ElevatedButton(
+                  child: Text(
+                    scanQRString,
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  onPressed: () => scanQR(),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+const String yourDoctorIsString = 'המטפל/ת שלך הוא/היא';
+const String doctorString = 'ד"ר יסמין כרמי';
+
+class DoctorInfoPage extends StatelessWidget {
+  final String doctorName;
+
+  DoctorInfoPage({Key key, this.doctorName}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: AppColors.mainYellow,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  yourDoctorIsString,
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+
+              LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(20),
+                      color: AppColors.mainWhite,
+                    ),
+                    height: MediaQuery.of(context).size.height / 3,
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(30.0),
+                          child: CircleAvatar(
+                            radius: 60,
+                            backgroundImage: NetworkImage('https://www.woolha.com/media/2020/03/eevee.png'),
+                          ),
+                        ),
+                        Text(
+                          doctorName,
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)
+                        ),
+                        Text(
+                          'מתמחה במחלקה הכירוגית',
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(20),
+                    primary: AppColors.buttonRed,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Icon(Icons.favorite),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
