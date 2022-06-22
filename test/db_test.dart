24e1e37d03f5573bd2141e9a35d02430625e4f51
shaiTheKimhi <shaikimhi221199@gmail.com>
2022-06-23 00:12:07 +0300

    preparing for mock and tests

--- test/db_test.dart
+++ test/db_test.dart
@@ -1,7 +1,33 @@
 import 'package:test/test.dart';
 import 'package:wellibe_proj/services/database.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+
+import 'package:wellibe_proj/services/auth.dart';
+
+
+class MDB extends Mock implements DB{
+
+}
+class MAuth extends Mock implements AuthService{
+  //No usage in DB so no need of override here
+}
+// ask about how exactly to override user collections and override value return functions
+class Users extends Mock {
+
+}
+
+
 
 void main(){
+
+  //create MockDB and mock user collection and use them in testing
+
+
+
+
 
   group('admin tests', () {
     DatabaseService db = new DatabaseService(uid: 'hospital@gmail.com');
